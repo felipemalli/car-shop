d03d5da3b34e9e1b9aa8ee67240c6b2de28535dc
felipemalli <felipevahia@gmail.com>
2022-07-13 20:46:48 -0300

    feat: implement routes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import CustomRouter from './routes/Router';
 import App from './app';
+import CustomRouter from './routes/Router';
 
 import CarController from './controllers/CarController';
+import { Car } from './interfaces/CarInterface';
 
 const server = new App();
 
-const exampleController = new exampleController();
+const carController = new CarController();
 
-const exampleRouter = new CustomRouter<Car>();
-exampleRouter.addRoute(exampleController);
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
 
-server.addRouter(exampleRouter.router);
+server.addRouter(carRouter.router);
 
 export default server;
