0b0050c5d945e1c179700f0a8df91f048a8e7ede
felipemalli <felipevahia@gmail.com>
2022-07-13 19:28:18 -0300

    refactor: change message importation name

--- src/utils/errors/NotFoundError.ts
+++ src/utils/errors/NotFoundError.ts
@@ -1,9 +1,9 @@
-import ResponseMessages from '../ResponseMessages';
+import Messages from '../Messages';
 import StatusCodes from '../StatusCodes';
 import CustomError from './CustomError';
 
 class NotFoundError extends CustomError {
-  constructor(message: string = ResponseMessages.OBJECT_NOT_FOUND) {
+  constructor(message: string = Messages.OBJECT_NOT_FOUND) {
     super(message, StatusCodes.NOT_FOUND);
   }
 }
