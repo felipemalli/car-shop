e1cafeefc8afd1bc67a37ca99be49626c7792bb7
felipemalli <felipevahia@gmail.com>
2022-07-13 19:27:23 -0300

    feat: add bad request message and remove not used messages

---
+++ src/utils/Messages.ts
@@ -0,0 +1,8 @@
+enum Messages {
+  HEXADECIMAL_ID = 'Id must have 24 hexadecimal characters',
+  OBJECT_NOT_FOUND = 'Object not found',
+  BAD_REQUEST_ERROR = 'Bad Request error',
+  INVALID_FIELDS = 'Invalid fields',
+}
+
+export default Messages;
