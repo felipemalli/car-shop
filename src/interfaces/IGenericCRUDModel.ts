1fb231345bdc8c844c6a569c853faa4da2bc0a1f
felipemalli <felipevahia@gmail.com>
2022-07-13 17:47:25 -0300

    feat: implement generic crud model interface

---
+++ src/interfaces/IGenericCRUDModel.ts
@@ -0,0 +1,11 @@
+export default interface IGenericModel<T> {
+  create(entity: T): Promise<T>;
+
+  read(): Promise<T[]>;
+  
+  readOne(id: string): Promise<T | null>;
+
+  update(id: string, entity: T): Promise<T | null>;
+
+  delete(id: string): Promise<T | null | void>;
+}
