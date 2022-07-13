15fabc0845d8492bb18dca8b37aab4edbb404880
felipemalli <felipevahia@gmail.com>
2022-07-13 17:48:33 -0300

    feat: implement model interface

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,11 @@
 export interface Model<T> {
   create(entity: T): Promise<T>;
-  read(): Promise<Array<T>>;
+
+  read(): Promise<T[]>;
+  
   readOne(id: string): Promise<T | null>;
+
   update(id: string, entity: T): Promise<T | null>;
-  delete(id: string): Promise<T | null>;
-} 
+
+  delete(id: string): Promise<void>;
+}
